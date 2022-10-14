6237cd81bb07f40dca5a775d5cb80284d3db2cd1
gemmabcr <77388323+gemmabcr@users.noreply.github.com>
2022-10-14 19:02:03 +0200

    feat: function calculate total (ex1)

--- pressupost/src/App.js
+++ pressupost/src/App.js
@@ -1,22 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
+  const title = '¿Qué quieres hacer?';
+  const data = [
+    {
+      name: 'web',
+      price: 500,
+      product: 'Una pàgina web',
+      selected: false,
+    },
+    {
+      name: 'seo',
+      price: 300,
+      product: 'Una campanya SEO',
+      selected: false,
+    },
+    {
+      name: 'ads',
+      price: 200,
+      product: 'Una campanya de Google Ads',
+      selected: false,
+    },
+  ]
+
+  const [formData, setFormData] = React.useState(data);
+  const [total, setTotal] = React.useState(0);
+
+  function handleChange(event) {
+    const {name, checked} = event.target
+    setFormData(prevFormData => {
+      const newFormData = []
+      for (let i = 0 ; i < prevFormData.length; i++){
+        const currentItem = prevFormData[i]
+        if (currentItem.name === name) {
+          const updatedItem = {
+            ...currentItem,
+            selected: checked,
+          }
+          newFormData.push(updatedItem)
+        }
+        else {
+          newFormData.push(currentItem)
+        }
+      }
+      return newFormData
+    })
+  }
+
+  React.useEffect(()=>{
+    let total = 0;
+    formData.map(item => {
+        if (item.selected) {
+          total += item.price
+        }
+      }
+    )
+    setTotal(total)
+  }, formData);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>{title}</h2>
+        {formData.map(item=>
+          <div key={item.name}>
+            <input
+              checked={item.selected}
+              id={item.name}
+              name={item.name}
+              type='checkbox'
+              onChange={handleChange}
+            />
+            <label htmlFor={item.name}>
+              {item.product}
+            </label>
+          </div>
+        )}
+        <p>Preu: {total}€</p>
       </header>
     </div>
   );
